d46d930ce29bae6f0f531a4110898bd94d1a9835
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-01-18 13:41:39 -0300

    link to home page

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import styled from 'styled-components';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 export default class NavBar extends Component {
     render() {
         return (
-            <div>
-                <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top" >
-                    <a className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center">Pokemón</a>
-                </nav>
-            </div>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top" >
+                <div className="container">
+                    <Link to="/" className="navbar-brand mr-0 col-md-5 col-sm-4 col-xs-6 navbar-link">Home</Link>
+                    <a className="navbar-brand mr-0 col-md-7 col-sm-8 col-xs-6">Pokemon - Challenge</a>
+                </div>
+            </nav>
         )
     }
 }
