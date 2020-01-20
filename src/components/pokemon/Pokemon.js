8c7a4ca153ca5e536183be2b91f35c2b6df18024
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-01-20 11:09:59 -0300

    evolution list starting to work

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -26,7 +26,7 @@ const TYPE_COLORS = {
 export default class Pokemon extends Component {
     state = {
         name: '',
-        pokeIndex: '',
+        index: '',
         imageUrl: '',
         types: [],
         description: '',
@@ -54,15 +54,13 @@ export default class Pokemon extends Component {
 
     async componentDidMount() {
         const { index } = this.props.match.params;
-        const pokeIndex = index;
         
         // Urls for pokemon information
-        const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokeIndex}/`;
-        const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokeIndex}/`;
+        const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${index}/`;
+        const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${index}/`;
     
         // Get Pokemon Information
-        const pokemonRes = await axios.get(pokemonUrl);
-        console.log(pokemonRes)
+        const pokemonRes = await axios.get(pokemonUrl,{headers: {'Access-Control-Allow-Origin': '*'}});
     
         const name = pokemonRes.data.name;
         const imageUrl = pokemonRes.data.sprites.front_default;
@@ -166,7 +164,7 @@ export default class Pokemon extends Component {
         
         this.setState({
             imageUrl,
-            pokeIndex,
+            index,
             name,
             types,
             stats: {
--- src/components/pokemon/evolution/EvolutionList.js
+++ src/components/pokemon/evolution/EvolutionList.js
@@ -1,46 +1,76 @@
 import React, { Component } from 'react'
 import Axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class EvolutionList extends Component {
     state = {
-        evolution: '',
-        pokemonIndex: ''
+        name: '',
+        evolutionOne: '',
+        evolutionTree: '',
+        pokemonEvolution1: ''
     }
 
     async componentDidMount() {
-        const { pokemonIndex } = this.props;
-    
+        const { index, name } = this.props;
+
         // Urls for pokemon information
-        const pokemonUrl = `https://pokeapi.co/api/v2/evolution-chain/${pokemonIndex}/`;
+        const speciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${index}/`;
 
-        const pokemonRes = await Axios.get(pokemonUrl);
-        console.log(pokemonRes)
+        const evolutionRes = await Axios.get(speciesUrl,{headers: {'Access-Control-Allow-Origin': '*'}});
 
-        const evolution = 'Name'
-            console.log(evolution)
+        const evolutionUrl = evolutionRes.data.evolution_chain.url
+        
+        const evolutionArray = await Axios.get(evolutionUrl)
+        
+        const evolutionOne = evolutionArray.data.chain.evolves_to.map(res => {
+            return res.species
+        })
+
+        const evolutionTwoComing = evolutionArray.data.chain.evolves_to.map(res => {
+            return res.evolves_to
+        })
+
+        const evolutionTwo = evolutionTwoComing.map(res => { return res[0].species })
+
+        const pokemonName = name;
+        console.log(pokemonName)
+        const pokemonEvolution1 = evolutionOne.map(name => { return name.name} );
+        const pokemonEvolution2 = evolutionTwo.map(name => { return name.name} );
+        console.log(pokemonEvolution1)
 
         this.setState({
-            evolution,
-            pokemonIndex
+            name, index, evolutionOne, evolutionTwo, pokemonEvolution1
         })
     }
 
     render() {
         return (
             <div className="card-body">
-                <h5 className="card-title text-center">Evolution List</h5>
-                <div className="row">
-                    <div className="col-md-6">
-                        <div className="row">Picture</div>
-                    </div>
-                    <div className="col-md-6">
-                        {this.state.evolution ? (
-                            <h6>{this.state.evolution}</h6>
-                        ) : (
-                            <h1> Don't have evolutions</h1>
-                        )}
-                    </div>
-                </div>
+                <h5 className="card-title text-center">Evolutions</h5>
+                { this.state.evolutionTwo ? (
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Link</th>
+                            </tr>
+                        </thead>
+                            <tbody>
+                                {this.state.evolutionOne.map(pokemon => (
+                                    <tr key={pokemon.url} >
+                                        <th scope="row">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</th>
+                                        <td><Link to={`${pokemon.url.split("/")[pokemon.url.split('/').length - 2]}`} target="_blank">Go to Page</Link></td>
+                                    </tr>
+                                ))}
+                                {this.state.evolutionTwo.map(pokemon => (
+                                    <tr key={pokemon.name} >
+                                        <th scope="row">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</th>
+                                        <td><Link to={`${pokemon.url.split("/")[pokemon.url.split('/').length - 2]}`} target="_blank">Go to Page</Link></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                    </table>
+                ): ( <h5>Don't have evolutions</h5> )}
             </div>
         )
     }
